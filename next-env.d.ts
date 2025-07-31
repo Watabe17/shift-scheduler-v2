431646b1e1f194dad5fbb5b3dfef73ab9f99b4fa
watabe17 <watabe.h.work@gmail.com>
2025-07-31 21:28:41 +0900

    fix: next-authの型定義を専用ファイルに分離

--- next-env.d.ts
+++ next-env.d.ts
@@ -3,15 +3,3 @@
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/app/api-reference/config/typescript for more information.
-
-import 'next-auth';
-import { DefaultSession } from 'next-auth';
-
-declare module 'next-auth' {
-  interface Session {
-    user: {
-      id: string;
-      role: string;
-    } & DefaultSession['user'];
-  }
-}
